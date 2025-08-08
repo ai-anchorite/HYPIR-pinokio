c62d663ac898e890e1fc73c8004552ff44d6287e
ai_anchorite <ai_anchorite@proton.me>
2025-08-09 00:53:11 +1200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# HYPIR
+
+A pinokio script for https://github.com/XPixelGroup/HYPIR.git
+
---
+++ install.js
@@ -0,0 +1,50 @@
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/ai-anchorite/HYPIR.git app",
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+          // triton: true   // uncomment this line if your project requires triton
+          // sageattention: true   // uncomment this line if your project requires sageattention
+        }
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "uv pip install gradio devicetorch",
+          "uv pip install -r requirements.txt",
+          "uv pip install hf_xet"
+        ]
+      }
+    },
+    
+    {
+      method: "hf.download",
+      params: {
+        path: "app",
+        "_": [ "lxq007/HYPIR" ],
+        "exclude": '".gitattributes" "*.md"',
+        "local-dir": "models",
+      }
+    },
+  ]
+}
